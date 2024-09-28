94d7f3635305235fb85f26b3f6ff973ed85b14b0
APysnack <60242065+APysnack@users.noreply.github.com>
2024-09-28 12:31:09 -0400

    wip

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -35,8 +35,12 @@ export default function RootLayout() {
     <Provider store={store}>
       <UserLoader />
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name='(tabs)' />
           <Stack.Screen name='+not-found' />
         </Stack>
       </ThemeProvider>
