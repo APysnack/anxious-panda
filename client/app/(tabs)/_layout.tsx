c084d6497dcdef29b4dee59ab3900c63eb367c6c
APysnack <60242065+APysnack@users.noreply.github.com>
2024-09-28 14:26:36 -0400

    wip

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -1,10 +1,34 @@
 import { Tabs } from 'expo-router';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const TabsLayout = () => {
   return (
     <Tabs>
-      <Tabs.Screen name='(index)/index' options={{ headerShown: false }} />
-      <Tabs.Screen name='game-room/[id]' options={{ headerShown: false }} />
+      <Tabs.Screen
+        name='(index)/index'
+        options={{
+          headerShown: false,
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name='home-outline' size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='game-room/[id]'
+        options={{
+          headerShown: false,
+          tabBarLabel: 'Game Room',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name='gamepad-variant'
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 };
