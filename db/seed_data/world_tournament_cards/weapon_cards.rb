c2e4ed411aa9590f61400c8e642d5b5010f37583
APysnack <60242065+APysnack@users.noreply.github.com>
2024-10-01 17:07:21 -0400

    add images

--- db/seed_data/world_tournament_cards/weapon_cards.rb
+++ db/seed_data/world_tournament_cards/weapon_cards.rb
@@ -3,81 +3,97 @@ def wt_weapon_cards
     {
       name: "Medicine Rod",
       sub_type: "Weapon",
+      front_image: "Medicine-Rod.jpg",
       data: { strength: 0, intellect: 5, agility: 0 }
     },
     {
       name: "Mace",
       sub_type: "Weapon",
+      front_image: "Mace.jpg",
       data: { strength: 3, intellect: 2, agility: 0 }
     },
     {
       name: "Long Bow",
       sub_type: "Weapon",
+      front_image: "Long-Bow.jpg",
       data: { strength: 0, intellect: 0, agility: 5 }
     },
     {
       name: "Scythe",
       sub_type: "Weapon",
+      front_image: "Scythe.jpg",
       data: { strength: 2, intellect: 3, agility: 0 }
     },
     {
       name: "Magic Orb",
       sub_type: "Weapon",
+      front_image: "Magic-Orb.jpg",
       data: { strength: 0, intellect: 5, agility: 0 }
     },
     {
       name: "Battle Flail",
       sub_type: "Weapon",
+      front_image: "Battle-Flail.jpg",
       data: { strength: 4, intellect: 0, agility: 1 }
     },
     {
       name: "Short Sword",
       sub_type: "Weapon",
+      front_image: "Short-Sword.jpg",
       data: { strength: 1, intellect: 0, agility: 4 }
     },
     {
       name: "War Hammer",
       sub_type: "Weapon",
+      front_image: "Warhammer.jpg",
       data: { strength: 4, intellect: 1, agility: 0 }
     },
     {
       name: "Battle Axe",
       sub_type: "Weapon",
+      front_image: "Battle-Axe.jpg",
       data: { strength: 4, intellect: 0, agility: 1 }
     },
     {
       name: "Spear",
       sub_type: "Weapon",
+      front_image: "Spear.jpg",
       data: { strength: 2, intellect: 0, agility: 3 }
     },
     {
       name: "Trident",
       sub_type: "Weapon",
+      front_image: "Trident.jpg",
       data: { strength: 1, intellect: 1, agility: 3 }
     },
     {
       name: "Daggers",
       sub_type: "Weapon",
+      front_image: "Daggers.jpg",
       data: { strength: 2, intellect: 0, agility: 3 }
     },
     {
       name: "Bastard Sword",
       sub_type: "Weapon",
+      front_image: "Bastard-Sword.jpg",
       data: { strength: 5, intellect: 0, agility: 0 }
     },
     {
       name: "Amulet",
       sub_type: "Weapon",
+      front_image: "Amulet.jpg",
       data: { strength: 0, intellect: 5, agility: 0 }
     },
     {
       name: "Crossbow",
       sub_type: "Weapon",
+      front_image: "Crossbow.jpg",
       data: { strength: 0, intellect: 0, agility: 5 }
     },
     {
       name: "Spiked Club",
       sub_type: "Weapon",
+      front_image: "Spiked-Club.jpg",
       data: { strength: 5, intellect: 0, agility: 0 }
     }
   ]
